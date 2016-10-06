5b3969d989e5f9f6edc9abbbe5fd0c9088bd3550
Andrew Morrison <morro141@gmail.com>
2016-10-06 07:38:50 -0400

    Finish static pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+	def full_title(title)
+		base_title = "RORT Sample App"
+		if (title.nil? || title.empty?)
+			base_title
+		else
+			title + " | " + base_title
+		end
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
-  get 'static_pages/home'
-
-  get 'static_pages/help'
-
-	root 'application#hello'
+  root 'static_pages#home'
+  get '/home' => 'static_pages#home', as: 'home'
+  get '/help' => 'static_pages#help', as: 'help'
+  get  '/about' => 'static_pages#about', as: 'about'
+  get  '/contact' =>'static_pages#contact', as: 'contact'
 
 end
