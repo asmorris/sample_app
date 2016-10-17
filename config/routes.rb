9043b2c9d086d8fcf74bf786aa10886a11c0ed8c
Andrew Morrison <morro141@gmail.com>
2016-10-16 23:31:18 -0400

    Changed routes, testing git status

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
 
-  get 'users/new'
-
   root 'static_pages#home'
   get '/home' => 'static_pages#home', as: 'home'
   get '/help' => 'static_pages#help', as: 'help'
   get  '/about' => 'static_pages#about', as: 'about'
   get  '/contact' =>'static_pages#contact', as: 'contact'
-
+  get '/signup', to: 'users#new'
 end
