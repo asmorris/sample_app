4b5a0db9169b3e2bc44c129837d02e9cd64d1996
Andrew Morrison <morro141@gmail.com>
2016-10-16 23:27:23 -0400

    Add users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'users/new'
+
   root 'static_pages#home'
   get '/home' => 'static_pages#home', as: 'home'
   get '/help' => 'static_pages#help', as: 'help'
